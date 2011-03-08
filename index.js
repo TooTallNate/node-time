7f38660b46a28625b9b87a324e9d1d2bd0516618
Nathan Rajlich <nathan@tootallnate.net>
2011-03-08 13:34:18 -0800

    Better user-facing 'tzset' function. It will now throw an Error on an invalid timezone String.

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 var bindings = require('./time');
 
 exports.time = bindings.time;
-exports.tzset = bindings.tzset;
 exports.localtime = bindings.localtime;
+
+// The user-facing 'tzset' function accepts a timezone String
+// to set to, and returns an object with the zoneinfo for the
+// timezone.
+function tzset(tz) {
+  if (tz) {
+    process.env.TZ = tz;
+  }
+  var usedTz = process.env.TZ;
+  var rtn = bindings.tzset();
+  if (!rtn.tzname[1] && rtn.timezone === 0) {
+    var err = new Error("Unknown Timezone: '" + usedTz + "'");
+    for (var i in rtn) {
+      err[i] = rtn[i];
+    }
+    throw err;
+  }
+  return rtn;
+}
+exports.tzset = tzset;
 
 var DateProto = global.Date.prototype;
 
@@ -11,15 +30,51 @@ var DateProto = global.Date.prototype;
 // 'getHours()', 'getDays()', etc. functions will return values relative
 // to the time zone specified.
 function setTimeZone(timezone) {
+  var oldTz = process.env.TZ;
+  tzset(timezone);
   this._timezone = timezone;
-  if (!('_getEpochSeconds' in this)) extend(this, extensions);
-  var origTz = process.env.TZ;
-  process.env.TZ = timezone;
-  bindings.tzset();
-  this._zoneInfo = bindings.localtime(this._getEpochSeconds());
-  process.env.TZ = origTz;
-  bindings.tzset();  
-  return this._zoneInfo;
+  var zoneInfo = bindings.localtime(this / 1000);
+  if (oldTz) {
+    tzset(oldTz);
+  }
+
+  // If we got to here without throwing an Error, then
+  // a valid timezone was requested, and we should have
+  // a valid zoneInfo Object.
+
+  // Returns the day of the month (1-31) for the specified date according to local time.
+  this.getDate = function getDate() {
+    return zoneInfo.dayOfMonth;
+  }
+  // Returns the day of the week (0-6) for the specified date according to local time.
+  this.getDay = function getDay() {
+    return zoneInfo.dayOfWeek;
+  }
+  // Deprecated. Returns the year (usually 2-3 digits) in the specified date according
+  // to local time. Use `getFullYear()` instead.
+  this.getYear = function getYear() {
+    return zoneInfo.year;
+  }
+  // Returns the year (4 digits for 4-digit years) of the specified date according to local time.
+  this.getFullYear = function getFullYear() {
+    return zoneInfo.year + 1900;
+  }
+  // Returns the hour (0-23) in the specified date according to local time.
+  this.getHours = function getHours() {
+    return zoneInfo.hours;
+  }
+  // Returns the minutes (0-59) in the specified date according to local time.
+  this.getMinutes = function getMinutes() {
+    return zoneInfo.minutes;
+  }
+  // Returns the month (0-11) in the specified date according to local time.
+  this.getMonth = function getMonth() {
+    return zoneInfo.month;
+  }
+  // Returns the seconds (0-59) in the specified date according to local time.
+  this.getSeconds = function getSeconds() {
+    return zoneInfo.seconds;
+  }
 }
 DateProto.setTimeZone = setTimeZone;
 
@@ -29,84 +84,3 @@ function getTimeZone() {
   return this._timezone;
 }
 DateProto.getTimeZone = getTimeZone;
-
-
-
-
-// These props gets monkey-patched onto a Date instance after the first time
-// the 'setTimeZone()' function is called on that instance.
-var extensions = {
-
-  // Like 'getTime', but returns seconds instead of milliseconds
-  _getEpochSeconds: function getEpochSeconds() {
-    return Math.floor(this.getTime() / 1000);
-  },
-
-  // 
-  // https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Date
-  //
-
-  // Returns the day of the month (1-31) for the specified date according to local time.
-  getDate: function getDate() {
-    return '_zoneInfo' in this ?
-      this._zoneInfo.dayOfMonth :
-      DateProto.getDate.call(this);
-  },
-
-  // Returns the day of the week (0-6) for the specified date according to local time.
-  getDay: function getDay() {
-    return '_zoneInfo' in this ?
-      this._zoneInfo.dayOfWeek :
-      DateProto.getDay.call(this);
-  },
-
-  // Deprecated. Returns the year (usually 2-3 digits) in the specified date according
-  // to local time. Use `getFullYear()` instead.
-  getYear: function getYear() {
-    return '_zoneInfo' in this ?
-      this._zoneInfo.year:
-      DateProto.getYear.call(this);
-  },
-
-  // Returns the year (4 digits for 4-digit years) of the specified date according to local time.
-  getFullYear: function getFullYear() {
-    return '_zoneInfo' in this ?
-      this._zoneInfo.year + 1900:
-      DateProto.getFullYear.call(this);
-  },
-
-  // Returns the hour (0-23) in the specified date according to local time.
-  getHours: function getHours() {
-    return '_zoneInfo' in this ?
-      this._zoneInfo.hours:
-      DateProto.getHours.call(this);
-  },
-
-  // Returns the minutes (0-59) in the specified date according to local time.
-  getMinutes: function getMinutes() {
-    return '_zoneInfo' in this ?
-      this._zoneInfo.minutes:
-      DateProto.getMinutes.call(this);
-  },
-
-  // Returns the month (0-11) in the specified date according to local time.
-  getMonth: function getMonth() {
-    return '_zoneInfo' in this ?
-      this._zoneInfo.month:
-      DateProto.getMonth.call(this);
-  },
-
-  // Returns the seconds (0-59) in the specified date according to local time.
-  getSeconds: function getSeconds() {
-    return '_zoneInfo' in this ?
-      this._zoneInfo.seconds:
-      DateProto.getSeconds.call(this);
-  }
-}
-
-
-function extend(dest, source) {
-  for (var prop in source) {
-    dest[prop] = source[prop];
-  }
-}
