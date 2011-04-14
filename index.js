3348c7d5be9cb53c9abf4fd883f4158a0b8b55a1
Clint Andrew Hall <clint@clintandrewhall.com>
2011-04-14 17:28:10 -0500

    Added new methods and minor formatting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var bindings = require('./time');
 
 exports.DAYS_OF_WEEK = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-exports.MONTHS = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+exports.MONTHS = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 exports.time = bindings.time;
 exports.localtime = bindings.localtime;
 
@@ -25,11 +25,13 @@ function tzset(tz) {
 }
 exports.tzset = tzset;
 
-// The "setTimezone" function is the "entry point" for a Date instance.
+var DateProto = global.Date.prototype;
+
+// The "setTimeZone" function is the "entry point" for a Date instance.
 // It must be called after an instance has been created. After, the 'getSeconds()',
 // 'getHours()', 'getDays()', etc. functions will return values relative
 // to the time zone specified.
-function setTimezone(timezone) {
+function setTimeZone(timezone) {
   var oldTz = process.env.TZ;
   var tz = tzset(timezone);
   var zoneInfo = bindings.localtime(this / 1000);
@@ -46,75 +48,76 @@ function setTimezone(timezone) {
   // Returns the day of the month (1-31) for the specified date according to local time.
   this.getDate = function getDate() {
     return zoneInfo.dayOfMonth;
-  }
+  };
   // Returns the day of the week (0-6) for the specified date according to local time.
   this.getDay = function getDay() {
     return zoneInfo.dayOfWeek;
-  }
+  };
   // Deprecated. Returns the year (usually 2-3 digits) in the specified date according
   // to local time. Use `getFullYear()` instead.
   this.getYear = function getYear() {
     return zoneInfo.year;
-  }
+  };
   // Returns the year (4 digits for 4-digit years) of the specified date according to local time.
   this.getFullYear = function getFullYear() {
     return zoneInfo.year + 1900;
-  }
+  };
   // Returns the hour (0-23) in the specified date according to local time.
   this.getHours = function getHours() {
     return zoneInfo.hours;
-  }
+  };
   // Returns the minutes (0-59) in the specified date according to local time.
   this.getMinutes = function getMinutes() {
     return zoneInfo.minutes;
-  }
+  };
   // Returns the month (0-11) in the specified date according to local time.
   this.getMonth = function getMonth() {
     return zoneInfo.month;
-  }
+  };
   // Returns the seconds (0-59) in the specified date according to local time.
   this.getSeconds = function getSeconds() {
     return zoneInfo.seconds;
-  }
-
-  this.getTimezoneOffset = function getTimezoneOffset() {
-    return -zoneInfo.gmtOffset / 60;
-  }
+  };
 
   this.toDateString = function toDateString() {
     return exports.DAYS_OF_WEEK[this.getDay()].substring(0, 3) + ' ' + exports.MONTHS[this.getMonth()].substring(0, 3) + ' ' + pad(this.getDate(), 2) + ' ' + this.getFullYear();
-  }
+  };
+  //Returns the abbreviation (e.g. EST, EDT) for the specified time zone.
+  this.getTimezoneAbbr = function getTimezoneAbbr() {
+    return tz.tzname[zoneInfo.isDaylightSavings ? 1 : 0];
+  };
+  // Returns the offset from UTC for the given timezone. This overrides the method inherited from Date, (as it is inaccurate).
+  this.getTimezoneOffset = function getTimezoneOffset() {
+    return -((zoneInfo.gmtOffset / 60 / 60) * 100);
+  };
 
   this.toTimeString = function toTimeString() {
-    var offset = zoneInfo.gmtOffset / 60 / 60;
-    return this.toLocaleTimeString() + ' GMT' + (offset >= 0 ? '+' : '-') + pad(Math.abs(offset * 100), 4)
-      + ' (' + tz.tzname[zoneInfo.isDaylightSavings ? 1 : 0] + ')';
-  }
+    var offset = this.getTimezoneOffset();
+    return this.toLocaleTimeString() + ' GMT' + (offset > 0 ? '-' : '+') + pad(offset, 4) + ' (' + this.getTimezoneAbbr() + ')';
+  };
 
   this.toString = function toString() {
     return this.toDateString() + ' ' + this.toTimeString();
-  }
+  };
 
   this.toLocaleDateString = function toLocaleDateString() {
     return exports.DAYS_OF_WEEK[this.getDay()] + ', ' + exports.MONTHS[this.getMonth()] + ' ' + pad(this.getDate(), 2) + ', ' + this.getFullYear();
-  }
+  };
 
   this.toLocaleTimeString = function toLocaleTimeString() {
     return pad(this.getHours(), 2) + ':' + pad(this.getMinutes(), 2) + ':' + pad(this.getSeconds(), 2);
-  }
+  };
 
   this.toLocaleString = this.toString;
 }
-Date.prototype.setTimezone = Date.prototype.setTimeZone = setTimezone;
+Date.prototype.setTimeZone = DateProto.setTimeZone = setTimeZone;
 
 
-// Returns a "String" of the last value set in "setTimezone".
-// TODO: Make this better. i.e. return something when 'setTimezone'
-//       hasn't been called yet.
-function getTimezone() {
+// Returns a "String" of the last value set in "setTimeZone".
+function getTimeZone() {
   return this._timezone;
 }
-Date.prototype.getTimezone = Date.prototype.getTimeZone = getTimezone;
+Date.prototype.getTimeZone = DateProto.getTimeZone = getTimeZone;
 
 // Export the modified 'Date' instance in case NODE_MODULE_CONTEXTS is set.
 exports.Date = Date;
