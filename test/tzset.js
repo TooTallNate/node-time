8f2d4e734b438cea6717a5a92d0b0bfae6489e7b
Nathan Rajlich <nathan@tootallnate.net>
2011-12-16 15:55:41 -0800

    Only set the TZ env var in beforeEach in tzset.js test.
    This way, we're not relying on behavior that this is testing.

--- test/tzset.js
+++ test/tzset.js
@@ -4,24 +4,21 @@ var should = require('should')
 describe('tzset()', function () {
 
   beforeEach(function () {
-    time.tzset('UTC')
+    process.env.TZ = 'UTC'
   })
 
   it('should work with no arguments', function () {
-    process.env.TZ.should.equal('UTC')
     process.env.TZ = 'US/Pacific'
     time.tzset()
     time.currentTimezone.should.equal('US/Pacific')
   })
 
   it('should work with 1 argument', function () {
-    process.env.TZ.should.equal('UTC')
     time.tzset('US/Pacific')
     time.currentTimezone.should.equal('US/Pacific')
   })
 
   it('should set `process.env.TZ`', function () {
-    process.env.TZ.should.equal('UTC')
     time.tzset('US/Pacific')
     process.env.TZ.should.equal('US/Pacific')
   })
