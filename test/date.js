01041998bbf32742f28651572f2ad8240bd31f73
Nathan Rajlich <nathan@tootallnate.net>
2013-11-18 23:53:50 -0800

    test: remove "should" usage from date.js

--- test/date.js
+++ test/date.js
@@ -3,7 +3,6 @@
  * Module dependencies.
  */
 
-require('should')
 var assert = require('assert')
 var time = require('../')
 
@@ -69,46 +68,46 @@ describe('Date', function () {
 
     var d = new time.Date(2000, 1, 2, 3, 4, 5, 6, 'America/Chicago')
     d.setFullYear(2001, 2, 3)
-    d.getTime().should.equal(983610245006)
+    assert.equal(d.getTime(), 983610245006)
     d.setFullYear(2002, 3)
-    d.getTime().should.equal(1017824645006)
+    assert.equal(d.getTime(), 1017824645006)
     d.setMonth(4, 5)
-    d.getTime().should.equal(1020585845006)
+    assert.equal(d.getTime(), 1020585845006)
     d.setHours(4, 5, 6, 7)
-    d.getTime().should.equal(1020589506007)
+    assert.equal(d.getTime(), 1020589506007)
     d.setHours(5, 6, 7)
-    d.getTime().should.equal(1020593167007)
+    assert.equal(d.getTime(), 1020593167007)
     d.setHours(6, 7)
-    d.getTime().should.equal(1020596827007)
+    assert.equal(d.getTime(), 1020596827007)
     d.setMinutes(8, 9, 10)
-    d.getTime().should.equal(1020596889010)
+    assert.equal(d.getTime(), 1020596889010)
     d.setMinutes(9, 10)
-    d.getTime().should.equal(1020596950010)
+    assert.equal(d.getTime(), 1020596950010)
     d.setSeconds(11, 12)
-    d.getTime().should.equal(1020596951012)
+    assert.equal(d.getTime(), 1020596951012)
   })
 
   it('should accept js1.3 extended setUTC* arguments', function() {
 
     var d = new time.Date(2000, 1, 2, 3, 4, 5, 6, 'America/Chicago')
     d.setUTCFullYear(2001, 2, 3)
-    d.getTime().should.equal(983610245006)
+    assert.equal(d.getTime(), 983610245006)
     d.setUTCFullYear(2002, 3)
-    d.getTime().should.equal(1017824645006)
+    assert.equal(d.getTime(), 1017824645006)
     d.setUTCMonth(4, 5)
-    d.getTime().should.equal(1020589445006)
+    assert.equal(d.getTime(), 1020589445006)
     d.setUTCHours(4, 5, 6, 7)
-    d.getTime().should.equal(1020571506007)
+    assert.equal(d.getTime(), 1020571506007)
     d.setUTCHours(5, 6, 7)
-    d.getTime().should.equal(1020575167007)
+    assert.equal(d.getTime(), 1020575167007)
     d.setUTCHours(6, 7)
-    d.getTime().should.equal(1020578827007)
+    assert.equal(d.getTime(), 1020578827007)
     d.setUTCMinutes(8, 9, 10)
-    d.getTime().should.equal(1020578889010)
+    assert.equal(d.getTime(), 1020578889010)
     d.setUTCMinutes(9, 10)
-    d.getTime().should.equal(1020578950010)
+    assert.equal(d.getTime(), 1020578950010)
     d.setUTCSeconds(11, 12)
-    d.getTime().should.equal(1020578951012)
+    assert.equal(d.getTime(), 1020578951012)
   })
 
   describe('#setTimezone()', function () {
@@ -121,34 +120,34 @@ describe('Date', function () {
       var initial = process.env.TZ
         , d = new time.Date()
       d.setTimezone('America/Argentina/San_Juan')
-      initial.should.equal(process.env.TZ)
+      assert.equal(initial, process.env.TZ)
     })
 
     it('should be chainable', function () {
       var initial = process.env.TZ
         , d = new time.Date().setTimezone('America/Argentina/San_Juan')
-      d.getTimezone().should.equal('America/Argentina/San_Juan')
+      assert.equal(d.getTimezone(), 'America/Argentina/San_Juan')
     })
 
     it('should change the "timezone offset"', function () {
       var d = new time.Date()
         , offset = d.getTimezoneOffset()
       d.setTimezone('US/Pacific')
-      d.getTimezoneOffset().should.not.equal(offset)
+      assert.notEqual(d.getTimezoneOffset(), offset)
     })
 
     it('should match the UTC values when set to "UTC"', function () {
       var d = new time.Date()
       d.setTimezone('UTC')
-      d.getUTCDay().should.equal(d.getDay())
-      d.getUTCDate().should.equal(d.getDate())
-      d.getUTCFullYear().should.equal(d.getFullYear())
-      d.getUTCHours().should.equal(d.getHours())
-      d.getUTCMilliseconds().should.equal(d.getMilliseconds())
-      d.getUTCMinutes().should.equal(d.getMinutes())
-      d.getUTCMonth().should.equal(d.getMonth())
-      d.getUTCSeconds().should.equal(d.getSeconds())
-      d.getTimezoneOffset().should.equal(0)
+      assert.equal(d.getUTCDay(), d.getDay())
+      assert.equal(d.getUTCDate(), d.getDate())
+      assert.equal(d.getUTCFullYear(), d.getFullYear())
+      assert.equal(d.getUTCHours(), d.getHours())
+      assert.equal(d.getUTCMilliseconds(), d.getMilliseconds())
+      assert.equal(d.getUTCMinutes(), d.getMinutes())
+      assert.equal(d.getUTCMonth(), d.getMonth())
+      assert.equal(d.getUTCSeconds(), d.getSeconds())
+      assert.equal(d.getTimezoneOffset(), 0)
     })
 
     it('should especially change the "hours" value', function () {
@@ -156,15 +155,15 @@ describe('Date', function () {
         , hours = d.getHours()
 
       d.setTimezone('US/Pacific')
-      d.getHours().should.not.equal(hours)
+      assert.notEqual(d.getHours(), hours)
       hours = d.getHours()
 
       d.setTimezone('US/Eastern')
-      d.getHours().should.not.equal(hours)
+      assert.notEqual(d.getHours(), hours)
       hours = d.getHours()
 
       d.setTimezone('America/Argentina/San_Juan')
-      d.getHours().should.not.equal(hours)
+      assert.notEqual(d.getHours(), hours)
     })
 
 
@@ -173,7 +172,7 @@ describe('Date', function () {
       it('should change the timezone', function () {
         var d = new time.Date()
         d.setTimezone('US/Pacific', true)
-        d.getTimezone().should.not.equal(process.env.TZ)
+        assert.notEqual(d.getTimezone(), process.env.TZ)
       })
 
       it('should keep local values', function () {
@@ -187,20 +186,20 @@ describe('Date', function () {
           , year = d.getFullYear()
         d.setTimezone('US/Pacific', true)
 
-        d.getMilliseconds().should.equal(millis)
-        d.getSeconds().should.equal(seconds)
-        d.getMinutes().should.equal(minutes)
-        d.getHours().should.equal(hours)
-        d.getDate().should.equal(date)
-        d.getMonth().should.equal(month)
-        d.getFullYear().should.equal(year)
+        assert.equal( d.getMilliseconds(), millis)
+        assert.equal(d.getSeconds(), seconds)
+        assert.equal(d.getMinutes(), minutes)
+        assert.equal(d.getHours(), hours)
+        assert.equal(d.getDate(), date)
+        assert.equal(d.getMonth(), month)
+        assert.equal(d.getFullYear(), year)
       })
 
       it('should change the date\'s internal time value', function () {
         var d = new time.Date()
           , old = d.getTime()
         d.setTimezone('US/Pacific', true)
-        d.getTime().should.not.equal(old)
+        assert.notEqual(d.getTime(), old)
       })
 
       it('should calculate correctly when UTC date is day after timezone date', function () {
@@ -212,34 +211,34 @@ describe('Date', function () {
           forwards.year, forwards.month - 1, forwards.date,
           forwards.hour, forwards.minute, 1, 1, forwards.timezone
         )
-        d.toString().should.equal('Thu Jan 31 2013 22:47:01 GMT-0800 (PST)')
+        assert.equal(d.toString(), 'Thu Jan 31 2013 22:47:01 GMT-0800 (PST)')
         d.setTimezone('UTC')
-        d.toString().should.equal('Fri Feb 01 2013 06:47:01 GMT+0000 (UTC)')
+        assert.equal(d.toString(), 'Fri Feb 01 2013 06:47:01 GMT+0000 (UTC)')
       })
 
       it('should calculate correctly when UTC date is day before timezone date', function () {
         var d = new time.Date(2010, 0, 31, 19, 0, 0, 0, 'UTC')
-        d.toString().should.equal('Sun Jan 31 2010 19:00:00 GMT+0000 (UTC)')
+        assert.equal(d.toString(), 'Sun Jan 31 2010 19:00:00 GMT+0000 (UTC)')
 
         var backwards = {
-            timezone: 'Australia/Sydney', hour: 2, minute: 47,
-            year: 2013, month: 2, date: 1
+          timezone: 'Australia/Sydney', hour: 2, minute: 47,
+          year: 2013, month: 2, date: 1
         }
 
         var d2 = new time.Date(
           backwards.year, backwards.month - 1, backwards.date,
           backwards.hour, backwards.minute, 1, 1, backwards.timezone
         )
-        d2.toString().should.equal('Fri Feb 01 2013 02:47:01 GMT+1100 (EST)')
+        assert.equal(d2.toString(), 'Fri Feb 01 2013 02:47:01 GMT+1100 (EST)')
         d2.setTimezone('UTC')
-        d2.toString().should.equal('Thu Jan 31 2013 15:47:01 GMT+0000 (UTC)')
+        assert.equal(d2.toString(), 'Thu Jan 31 2013 15:47:01 GMT+0000 (UTC)')
       })
 
       it('should calculate correctly on edge of months', function() {
         var d = new time.Date("2013-02-01 01:02:03", 'US/Pacific')
-        d.toString().should.equal('Fri Feb 01 2013 01:02:03 GMT-0800 (PST)')
+        assert.equal(d.toString(), 'Fri Feb 01 2013 01:02:03 GMT-0800 (PST)')
         d.setTimezone('UTC')
-        d.toString().should.equal('Fri Feb 01 2013 09:02:03 GMT+0000 (UTC)')
+        assert.equal(d.toString(), 'Fri Feb 01 2013 09:02:03 GMT+0000 (UTC)')
       })
 
     })
