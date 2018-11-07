7eae520cbb5f0f458e6c76e9779c65118d73de12
Andrey Melikhov <amel.truelife@gmail.com>
2018-11-07 23:14:43 +0000

    Migrate from NAN to N-API

--- src/time.cc
+++ src/time.cc
@@ -2,109 +2,117 @@
 #include <stdint.h>
 #include <time.h>
 
-#include "nan.h"
+#include "napi.h"
+#include "uv.h"
+#include "math.h"
 
-using namespace node;
-using namespace v8;
+using namespace Napi;
 
 class Time {
   public:
-  static void Init(Handle<Object> target) {
-    Nan::HandleScope scope;
+  static Napi::Object Init(Napi::Env env, Napi::Object exports) {
+    Napi::HandleScope scope(env);
 
     // time(3)
-    Nan::SetMethod(target, "time", Time_);
+    exports.Set(Napi::String::New(env, "time"), Napi::Function::New(env, Time_));
 
     // tzset(3)
-    Nan::SetMethod(target, "tzset", Tzset);
+    exports.Set(Napi::String::New(env, "tzset"), Napi::Function::New(env, Tzset));
 
     // localtime
-    Nan::SetMethod(target, "localtime", Localtime);
+    exports.Set(Napi::String::New(env, "localtime"), Napi::Function::New(env, Localtime));
 
     // mktime
-    Nan::SetMethod(target, "mktime", Mktime);
+    exports.Set(Napi::String::New(env, "mktime"), Napi::Function::New(env, Mktime));
+
+    return exports;
   }
 
-  static NAN_METHOD(Time_) {
-    Nan::EscapableHandleScope scope;
-    info.GetReturnValue().Set(scope.Escape(Nan::New<v8::Number>(time(NULL))));
+  static Napi::Value Time_(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+    Napi::EscapableHandleScope scope(env);
+    return scope.Escape(Napi::Number::New(env, time(NULL)));
   }
 
-  static NAN_METHOD(Tzset) {
-    Nan::EscapableHandleScope scope;
+  static Napi::Value Tzset(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+    Napi::EscapableHandleScope scope(env);
 
     // Set up the timezone info from the current TZ environ variable
     tzset();
 
     // Set up a return object that will hold the results of the timezone change
-    Local<Object> obj = Nan::New<v8::Object>();
+    Napi::Object obj = Napi::Object::New(env);
 
     // The 'tzname' char * [] gets put into a JS Array
     int tznameLength = 2;
-    Local<Array> tznameArray = Nan::New<v8::Array>( tznameLength );
+    Napi::Array tznameArray = Napi::Array::New(env,  tznameLength );
 #if _MSC_VER >= 1900
 	char szTzName[128];
 
 	for (int i = 0; i < tznameLength; i++) {
 		size_t strLength;
 		_get_tzname(&strLength, szTzName, 128, i);
-		Nan::Set(tznameArray, i, Nan::New<v8::String>(szTzName).ToLocalChecked());
+		(tznameArray).Set(i, Napi::String::New(env, szTzName));
 	}
 
-	Nan::Set(obj, Nan::New("tzname").ToLocalChecked(), tznameArray);
+	(obj).Set(Napi::String::New(env, "tzname"), tznameArray);
 
 	// The 'timezone' long is the "seconds West of UTC"
 	long timezone;
 	_get_timezone(&timezone);
-	Nan::Set(obj, Nan::New("timezone").ToLocalChecked(), Nan::New<v8::Number>(timezone));
+	(obj).Set(Napi::String::New(env, "timezone"), Napi::Number::New(env, timezone));
 
 	// The 'daylight' int is obselete actually, but I'll include it here for
 	// curiosity's sake. See the "Notes" section of "man tzset"
 	int daylight;
 	_get_daylight(&daylight);
-	Nan::Set(obj, Nan::New("daylight").ToLocalChecked(), Nan::New<v8::Number>(daylight));
+	(obj).Set(Napi::String::New(env, "daylight"), Napi::Number::New(env, daylight));
 #else
     for (int i=0; i < tznameLength; i++) {
-      Nan::Set(tznameArray, i, Nan::New<v8::String>(tzname[i]).ToLocalChecked());
+      (tznameArray).Set(i, Napi::String::New(env, tzname[i]));
     }
 
-    Nan::Set(obj, Nan::New("tzname").ToLocalChecked(), tznameArray);
+    (obj).Set(Napi::String::New(env, "tzname"), tznameArray);
 
     // The 'timezone' long is the "seconds West of UTC"
-    Nan::Set(obj, Nan::New("timezone").ToLocalChecked(), Nan::New<v8::Number>( timezone ));
+    (obj).Set(Napi::String::New(env, "timezone"), Napi::Number::New(env,  timezone ));
 
     // The 'daylight' int is obselete actually, but I'll include it here for
     // curiosity's sake. See the "Notes" section of "man tzset"
-    Nan::Set(obj, Nan::New("daylight").ToLocalChecked(), Nan::New<v8::Number>( daylight ));
+    (obj).Set(Napi::String::New(env, "daylight"), Napi::Number::New(env,  daylight ));
 #endif
-    info.GetReturnValue().Set(scope.Escape(obj));
+    return scope.Escape(obj);
   }
 
-  static NAN_METHOD(Localtime) {
-    Nan::EscapableHandleScope scope;
+  static Napi::Value Localtime(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+    Napi::EscapableHandleScope scope(env);
 
     // Construct the 'tm' struct
-    time_t rawtime = static_cast<time_t>(info[0]->IntegerValue());
+    time_t rawtime = static_cast<time_t>(info[0].As<Napi::Number>().Int64Value());
+    
     struct tm *timeinfo = localtime( &rawtime );
 
     // Create the return "Object"
-    Local<Object> obj = Nan::New<v8::Object>();
+    Napi::Object obj = Napi::Object::New(env);
 
-    if (timeinfo) {
-      Nan::Set(obj, Nan::New("seconds").ToLocalChecked(), Nan::New<v8::Integer>(timeinfo->tm_sec) );
-      Nan::Set(obj, Nan::New("minutes").ToLocalChecked(), Nan::New<v8::Integer>(timeinfo->tm_min) );
-      Nan::Set(obj, Nan::New("hours").ToLocalChecked(), Nan::New<v8::Integer>(timeinfo->tm_hour) );
-      Nan::Set(obj, Nan::New("dayOfMonth").ToLocalChecked(), Nan::New<v8::Integer>(timeinfo->tm_mday) );
-      Nan::Set(obj, Nan::New("month").ToLocalChecked(), Nan::New<v8::Integer>(timeinfo->tm_mon) );
-      Nan::Set(obj, Nan::New("year").ToLocalChecked(), Nan::New<v8::Integer>(timeinfo->tm_year) );
-      Nan::Set(obj, Nan::New("dayOfWeek").ToLocalChecked(), Nan::New<v8::Integer>(timeinfo->tm_wday) );
-      Nan::Set(obj, Nan::New("dayOfYear").ToLocalChecked(), Nan::New<v8::Integer>(timeinfo->tm_yday) );
-      Nan::Set(obj, Nan::New("isDaylightSavings").ToLocalChecked(), Nan::New<v8::Boolean>(timeinfo->tm_isdst > 0) );
+    if (timeinfo && !isnan(info[0].As<Napi::Number>().DoubleValue())) {
+      (obj).Set(Napi::String::New(env, "test"), Napi::Number::New(env, rawtime) );
+      (obj).Set(Napi::String::New(env, "seconds"), Napi::Number::New(env, timeinfo->tm_sec) );
+      (obj).Set(Napi::String::New(env, "minutes"), Napi::Number::New(env, timeinfo->tm_min) );
+      (obj).Set(Napi::String::New(env, "hours"), Napi::Number::New(env, timeinfo->tm_hour) );
+      (obj).Set(Napi::String::New(env, "dayOfMonth"), Napi::Number::New(env, timeinfo->tm_mday) );
+      (obj).Set(Napi::String::New(env, "month"), Napi::Number::New(env, timeinfo->tm_mon) );
+      (obj).Set(Napi::String::New(env, "year"), Napi::Number::New(env, timeinfo->tm_year) );
+      (obj).Set(Napi::String::New(env, "dayOfWeek"), Napi::Number::New(env, timeinfo->tm_wday) );
+      (obj).Set(Napi::String::New(env, "dayOfYear"), Napi::Number::New(env, timeinfo->tm_yday) );
+      (obj).Set(Napi::String::New(env, "isDaylightSavings"), Napi::Boolean::New(env, timeinfo->tm_isdst > 0) );
 
 #if defined HAVE_TM_GMTOFF
       // Only available with glibc's "tm" struct. Most Linuxes, Mac OS X...
-      Nan::Set(obj, Nan::New("gmtOffset").ToLocalChecked(), Nan::New<v8::Number>(timeinfo->tm_gmtoff) );
-      Nan::Set(obj, Nan::New("timezone").ToLocalChecked(), Nan::New(timeinfo->tm_zone).ToLocalChecked() );
+      (obj).Set(Napi::String::New(env, "gmtOffset"), Napi::Number::New(env, timeinfo->tm_gmtoff) );
+      (obj).Set(Napi::String::New(env, "timezone"), Napi::String::New(env, timeinfo->tm_zone) );
 
 #elif defined HAVE_TIMEZONE
       // Compatibility for Cygwin, Solaris, probably others...
@@ -118,43 +126,45 @@ class Time {
       } else {
         scd = -timezone;
       }
-      Nan::Set(obj, Nan::New("gmtOffset").ToLocalChecked(), Nan::New<v8::Integer>(scd));
-      Nan::Set(obj, Nan::New("timezone").ToLocalChecked(), Nan::New<v8::String>(tzname[timeinfo->tm_isdst]));
+      (obj).Set(Napi::String::New(env, "gmtOffset"), Napi::Number::New(env, scd));
+      (obj).Set(Napi::String::New(env, "timezone"), Napi::String::New(env, tzname[timeinfo->tm_isdst]));
 #endif // HAVE_TM_GMTOFF
     } else {
-      Nan::Set(obj, Nan::New("invalid").ToLocalChecked(), Nan::New<v8::Boolean>(true));
+      (obj).Set(Napi::String::New(env, "invalid"), Napi::Boolean::New(env, true));
     }
 
-    info.GetReturnValue().Set(scope.Escape(obj));
+    return scope.Escape(obj);
   }
 
-  static NAN_METHOD(Mktime) {
-    Nan::EscapableHandleScope scope;
+  static Napi::Value Mktime(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+    Napi::EscapableHandleScope scope(env);
 
     if (info.Length() < 1) {
-      return Nan::ThrowTypeError("localtime() Object expected");
+      Napi::TypeError::New(env, "localtime() Object expected").ThrowAsJavaScriptException();
+      return env.Null();
     }
 
-    Local<Object> arg = info[0].As<v8::Object>();
+    Napi::Object arg = info[0].As<Napi::Object>();
 
     struct tm tmstr;
-    tmstr.tm_sec   = Nan::Get(arg, Nan::New("seconds").ToLocalChecked()).ToLocalChecked()->Int32Value();
-    tmstr.tm_min   = Nan::Get(arg, Nan::New("minutes").ToLocalChecked()).ToLocalChecked()->Int32Value();
-    tmstr.tm_hour  = Nan::Get(arg, Nan::New("hours").ToLocalChecked()).ToLocalChecked()->Int32Value();
-    tmstr.tm_mday  = Nan::Get(arg, Nan::New("dayOfMonth").ToLocalChecked()).ToLocalChecked()->Int32Value();
-    tmstr.tm_mon   = Nan::Get(arg, Nan::New("month").ToLocalChecked()).ToLocalChecked()->Int32Value();
-    tmstr.tm_year  = Nan::Get(arg, Nan::New("year").ToLocalChecked()).ToLocalChecked()->Int32Value();
-    tmstr.tm_isdst = Nan::Get(arg, Nan::New("isDaylightSavings").ToLocalChecked()).ToLocalChecked()->Int32Value();
+    tmstr.tm_sec   = (arg).Get(Napi::String::New(env, "seconds")).As<Napi::Number>().Int32Value();
+    tmstr.tm_min   = (arg).Get(Napi::String::New(env, "minutes")).As<Napi::Number>().Int32Value();
+    tmstr.tm_hour  = (arg).Get(Napi::String::New(env, "hours")).As<Napi::Number>().Int32Value();
+    tmstr.tm_mday  = (arg).Get(Napi::String::New(env, "dayOfMonth")).As<Napi::Number>().Int32Value();
+    tmstr.tm_mon   = (arg).Get(Napi::String::New(env, "month")).As<Napi::Number>().Int32Value();
+    tmstr.tm_year  = (arg).Get(Napi::String::New(env, "year")).As<Napi::Number>().Int32Value();
+    tmstr.tm_isdst = (arg).Get(Napi::String::New(env, "isDaylightSavings")).As<Napi::Number>().Int32Value();
     // tm_wday and tm_yday are ignored for input, but properly set after 'mktime' is called
 
-    info.GetReturnValue().Set(scope.Escape(Nan::New<v8::Number>(static_cast<double>(mktime( &tmstr )))));
+    return scope.Escape(Napi::Number::New(env, static_cast<double>(mktime( &tmstr ))));
   }
 
 };
 
 extern "C" {
-  static void init (Handle<Object> target) {
-    Time::Init(target);
+  static Napi::Object init (Napi::Env env, Napi::Object exports) {
+    return Time::Init(env, exports);
   }
-  NODE_MODULE(time, init)
+  NODE_API_MODULE(time, init)
 }
